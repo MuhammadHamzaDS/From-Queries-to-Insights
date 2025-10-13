6bb4778b4c7e18c6a2b0442839c2ca3cca568240
MuhammadHamzaDS <hamzaali709x@gmail.com>
2025-10-12 21:23:55 -0700

    added student table creation script

---
+++ MYSQL_practice.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE mydb;
+USE mydb;
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT,
+    City VARCHAR(50)
+);
+describe Students;
+Show tables;
+CREATE TABLE Hamza (
+Roll_No INT primary Key,
+Name varchar(30),
+Age INT,
+City varchar(30)
+) ;
+describe hamza;
+insert into Hamza (Roll_No,Name,Age,City)
+values(1,"Muhammad Hamza", 22,'Sahiwal');
+select * from Hamza;
